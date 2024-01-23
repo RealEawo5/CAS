aea6734446508209e09cf85ce40693102f3a15fa
Real <53586056+RealEawo5@users.noreply.github.com>
2024-01-23 11:28:33 +0100

    a

--- updater.py
+++ updater.py
@@ -1,19 +1,49 @@
-import subprocess
+from packaging import version
+import requests
 import os
+import sys
+import subprocess
+
+
+CURRENT_VERSION = 'v1.0.1'
+
 
 def checkUpdates():
-    # Navigate to the directory where your program is located
-    os.chdir('.')
+    response = requests.get('https://api.github.com/repos/realeawo5/CAS/releases/latest')
+    LATEST_VERSION = response.json()['tag_name']
 
-    # Fetch the latest updates from the remote repository
-    subprocess.call(['git', 'fetch'])
-
-    # Check if there are any updates
-    result = subprocess.check_output(['git', 'status', '-uno'])
-
-    if 'Your branch is behind' in result.decode('utf-8'):
+    if isNewLocalRelease(LATEST_VERSION, CURRENT_VERSION):
         print('Update available. Updating now...')
-        # Pull the latest updates
-        subprocess.call(['git', 'pull'])
+        downloadUrl = response.json()['assets'][0]['browser_download_url']
+        response = requests.get(downloadUrl, stream=True)
+        with open('new-cas.exe', 'wb') as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+
+        # Replace the current .exe with the new one
+        os.rename('new-cas.exe', 'cas.exe')
+
+        # Restart the application
+        subprocess.Popen(['cas.exe'])
+        sys.exit()
+
+
+def isNewRelease(LATEST_VERSION, CURRENT_VERSION):
+    LATEST_VERSION = version.parse(LATEST_VERSION)
+    CURRENT_VERSION = version.parse(CURRENT_VERSION)
+
+    if CURRENT_VERSION > LATEST_VERSION:
+        return True
     else:
-        print('Your program is up to date.')
+        return False
+
+
+if __name__ == '__main__':
+    LATEST_VERSION = requests.get('https://api.github.com/repos/realeawo5/CAS/releases/latest').json()['tag_name']
+
+    print(isNewRelease(LATEST_VERSION, CURRENT_VERSION), end=":")
+    # print(LATEST_VERSION, end=":")
+    print(CURRENT_VERSION)
+
+    # checkUpdates()
